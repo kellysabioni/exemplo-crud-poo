7ca6f3fec4b6bed11ff02ac0078b3773349a6777
kellysabioni <kelly.sabioni@yahoo.com.br>
2025-05-13 10:09:19 -0300

    Cria branch 04

--- src/Services/FabricanteServico.php
+++ src/Services/FabricanteServico.php
@@ -1,12 +1,15 @@
 <?php
+
 namespace ExemploCrud\Services;
 
 use Exception;
 use ExemploCrud\Database\ConexaoBD;
+use ExemploCrud\Models\Fabricante;
 use PDO;
 use Throwable;
 
-final class FabricanteServico {
+final class FabricanteServico
+{
     private PDO $conexao;
 
     public function __construct()
@@ -14,14 +17,75 @@ final class FabricanteServico {
         $this->conexao = ConexaoBD::getConexao();
     }
 
-    public function listarTodos():array {
+    public function listarTodos(): array
+    {
         $sql = "SELECT * FROM fabricantes ORDER BY nome";
         try {
             $consulta = $this->conexao->prepare($sql);
             $consulta->execute();
             return $consulta->fetchAll(PDO::FETCH_ASSOC);
         } catch (Throwable $erro) {
-            throw new Exception("Erro ao carregar fabricantes: ".$erro->getMessage());
-        } 
+            throw new Exception("Erro ao carregar fabricantes: " . $erro->getMessage());
+        }
     }
-}
+
+    public function inserir(Fabricante $fabricante): void
+    {
+        $sql = "INSERT INTO fabricantes(nome) VALUES(:nome)";
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":nome", $fabricante->getNome(), PDO::PARAM_STR);
+            $consulta->execute();
+        } catch (Throwable $erro) {
+            throw new Exception("Erro ao inserir: " . $erro->getMessage());
+        }
+    }
+
+    public function buscarPorId(int $id): ?array
+    {
+        $sql = "SELECT * FROM fabricantes WHERE id = :id";
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $id, PDO::PARAM_INT);
+            $consulta->execute();
+
+            /* Guardamos o resultado da operação fetch em uma variável */
+            // $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+
+            /* Se o resultado for verdadeiro, retornamos ele. Senão, retornamos null */
+            // return $resultado ? $resultado : null;
+
+            // Versão usando ternário simplificado usando 'elvis operator'
+            return $consulta->fetch(PDO::FETCH_ASSOC) ?: null;
+        } catch (Throwable $erro) {
+            throw new Exception("Erro ao carregar fabricante: " . $erro->getMessage());
+        }
+    }
+
+    public function atualizar(Fabricante $fabricante): void
+    {
+        $sql = "UPDATE fabricantes SET nome = :nome WHERE id = :id";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":nome", $fabricante->getNome(), PDO::PARAM_STR);
+            $consulta->bindValue(":id", $fabricante->getId(), PDO::PARAM_INT);
+            $consulta->execute();
+        } catch (Throwable $erro) {
+            throw new Exception("Erro ao atualizar fabricante: " . $erro->getMessage());
+        }
+    }
+
+    public function excluir(int $id): void
+    {
+        $sql = "DELETE FROM fabricantes WHERE id = :id";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $id, PDO::PARAM_INT);
+            $consulta->execute();
+        } catch (Throwable $erro) {
+            throw new Exception("Erro ao excluir fabricante: " . $erro->getMessage());
+        }
+    }
+} // final da classe
--- src/funcoes-fabricantes.php
+++ src/funcoes-fabricantes.php
@@ -4,53 +4,13 @@ require_once "conecta.php";
 /* Lógica/Funções para o CRUD de Fabricantes */
 
 
-function listarFabricantes(PDO $conexao):array {
-    $sql = "SELECT * FROM fabricantes ORDER BY nome";
-
-    try {
-        $consulta = $conexao->prepare($sql);
-        $consulta->execute();
-        return $consulta->fetchAll(PDO::FETCH_ASSOC);
-    } catch (Exception $erro) {
-        die("Erro: ".$erro->getMessage());
-    }   
-}
-
-
-// inserirFabricante: usada pela página fabricantes/inserir.php
-                                                        // void indica que não tem retorno
-function inserirFabricante(PDO $conexao, string $nomeDoFabricante):void { 
-    /* :named parameter (parâmetro nomeado)
-    Usamos este recurso do PDO para 'reservar' um espaço seguro
-    em memória para colocação do dado. NUNCA passe de forma direta
-    valores para comandos SQL. */
-    $sql = "INSERT INTO fabricantes(nome) VALUES(:nome)";
-
-    try {
-        $consulta = $conexao->prepare($sql);
-        /* bindValue() -> permite vincular o valor
-        do parâmetro à consulta que será executada. É necessário 
-        indicar qual é o parâmetro (:nome), de onde vem o valor 
-        ($nomeDoFabricante) e de que tipo ele é (PDO:PARAM_STR) */
-        $consulta->bindValue(":nome", $nomeDoFabricante, PDO::PARAM_STR);        
-        $consulta->execute();
-    } catch (Exception $erro) {
-        die("Erro ao inserir: ".$erro->getMessage());
-    }
-}
-
-
 // listarUmFabricante: usada pela página fabricantes/atualizar.php
 function listarUmFabricante(PDO $conexao, int $idFabricante):array {
     $sql = "SELECT * FROM fabricantes WHERE id = :id";
-
     try {
         $consulta = $conexao->prepare($sql);
         $consulta->bindValue(":id", $idFabricante, PDO::PARAM_INT);
         $consulta->execute();
-
-        /* Usamos o fetch para garantir o retorno
-        de um único array associativo com o resultado */
         return $consulta->fetch(PDO::FETCH_ASSOC);
     } catch (Exception $erro) {
         die("Erro ao carregar fabricante: ".$erro->getMessage());
